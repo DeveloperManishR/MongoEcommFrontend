997c7562c4307ac2966bfebb94c8d82664e08b34
Manish Rawat <manishrawat@Manishs-MacBook-Pro.local>
2025-07-07 21:06:44 +0530

    first commit

---
+++ src/config/config.tsx
@@ -0,0 +1,13 @@
+import axios, { AxiosInstance } from "axios";
+
+
+export const authAxios = (token?: string): AxiosInstance => {
+    
+    return axios.create({
+      
+       baseURL:`${import.meta.env.VITE_BASE_URL}/api/admin`,
+        headers: {
+            'Authorization': `${token ? `${token}` : ''}`,
+        },
+    });
+};
